a6883170f8f3c3aaa8c53cf0a5e984055dc46887
Jelle Dekker <jelle.dekker@bizzomate.com>
2017-02-09 09:15:57 +0100

    Initital commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Description
+This widget implements the Google Timeline Chart in Mendix. An efficient way to display (and possibly edit) time related entries in your domain model. Also see https://developers.google.com/chart/interactive/docs/gallery/timeline for more information on the functionalities and limitations of the Google Timeline Chart.
+
+##Typical usage scenario
+Use this widget to render time related entries for the user. This can be handy for example when the user needs to get a quick overview of possible overlaps or otherwise conflicting data or to pinpoint when there is a timeslot available.
+
+##Features and limitations
+* Allows rendering of and interaction with time related data
+* Easy configuration of the timeline using widget settings
+* Can be used with persistent and non persistent entities
+* Data retrieval over association is possible
+* The chart is always loaded from the Google servers using the Google Loader, internet connection is required
+
+
+##Dependencies
+* Build and tested using Mendix 6.10.2
---
+++ src/GoogleChartTimeline/widget/GoogleChartTimeline.js
@@ -0,0 +1,383 @@
+/*global logger*/
+/*
+    GoogleChartTimeline
+    ========================
+
+    @file      : GoogleChartTimeline.js
+    @version   : 1.0.0
+    @author    : Jelle Dekker
+    @date      : 2/8/2017
+    @copyright : Bizzomate 2017
+    @license   : Apache 2
+
+    Documentation
+    ========================
+    Describe your widget here.
+*/
+
+// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+define([
+  "dojo/_base/declare",
+  "mxui/widget/_WidgetBase",
+  "dijit/_TemplatedMixin",
+
+  "mxui/dom",
+  "dojo/dom",
+  "dojo/dom-prop",
+  "dojo/dom-geometry",
+  "dojo/dom-class",
+  "dojo/dom-style",
+  "dojo/dom-construct",
+  "dojo/_base/array",
+  "dojo/_base/lang",
+  "dojo/text",
+  "dojo/html",
+  "dojo/_base/event",
+
+  "dojo/query",
+
+  "GoogleChartTimeline/lib/loader",
+  "dojo/text!GoogleChartTimeline/widget/template/GoogleChartTimeline.html"
+], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoEvent, dojoQuery, loader, widgetTemplate) {
+  "use strict";
+
+  // Declare widget's prototype.
+  return declare("GoogleChartTimeline.widget.GoogleChartTimeline", [_WidgetBase, _TemplatedMixin], {
+    // _TemplatedMixin will create our dom node using this HTML template.
+    templateString: widgetTemplate,
+
+    // Parameters configured in the Modeler un.
+    dataItem: "",
+    rowLabel: "",
+    barLabel: "",
+    tooltip: "",
+    barStart: "",
+    barEnd: "",
+    dataRetrieveMF: "",
+
+    maxHeight: "",
+    barColor: "",
+    backgroundColor: "",
+    showTooltips: "",
+    colorByRowLabel: "",
+    groupByRowLabel: "",
+    showRowLabels: "",
+    showBarLabels: "",
+
+    onSelectMF: "",
+
+    chartVersion: "",
+    chartLanguage: "",
+
+    // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+    _handles: null,
+    _contextObj: null,
+    _alertDiv: null,
+    _readOnly: false,
+    _dataTable: null,
+    _options: null,
+    _chart: null,
+    _rowItems: null,
+    _itemsLoaded: null,
+
+    // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+    constructor: function() {
+      //logger.level(logger.DEBUG);
+      logger.debug(this.id + ".constructor");
+      this._handles = [];
+    },
+
+    // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+    postCreate: function() {
+      logger.debug(this.id + ".postCreate");
+
+      if (this.readOnly || this.get("disabled") || this.readonly) {
+        this._readOnly = true;
+      }
+
+      this._updateRendering();
+    },
+
+    // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+    update: function(obj, callback) {
+      logger.debug(this.id + ".update");
+
+      this._contextObj = obj;
+      this._resetSubscriptions();
+      this._updateRendering(callback); // We're passing the callback to updateRendering to be called after DOM-manipulation
+    },
+
+    // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
+    enable: function() {
+      logger.debug(this.id + ".enable");
+    },
+
+    // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
+    disable: function() {
+      logger.debug(this.id + ".disable");
+    },
+
+    // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
+    resize: function(box) {
+      logger.debug(this.id + ".resize");
+      if (this._chart) {
+        this._drawChart();
+      }
+    },
+
+    // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+    uninitialize: function() {
+      logger.debug(this.id + ".uninitialize");
+      // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
+    },
+
+    // We want to stop events on a mobile device
+    _stopBubblingEventOnMobile: function(e) {
+      logger.debug(this.id + "._stopBubblingEventOnMobile");
+      if (typeof document.ontouchstart !== "undefined") {
+        dojoEvent.stop(e);
+      }
+    },
+
+    // Rerender the interface.
+    _updateRendering: function(callback) {
+      logger.debug(this.id + "._updateRendering");
+
+      if (this._contextObj) {
+        dojoStyle.set(this.domNode, "display", "block");
+        dojoStyle.set(this.domNode, "height", this.maxHeight + 'px');
+
+        if (!google.visualization || !google.visualization.Timeline) {
+          this._loadGoogleTimelineChart();
+        } else {
+          this._getChartData();
+        }
+      } else {
+        dojoStyle.set(this.domNode, "display", "none");
+      }
+
+      // The callback, coming from update, needs to be executed, to let the page know it finished rendering
+      mendix.lang.nullExec(callback);
+    },
+
+    //Use the Google Loader script to get the provided version of the Google Timeline chart
+    _loadGoogleTimelineChart: function() {
+      logger.debug(this.id + "._loadGoogleTimelineChart");
+      google.charts.load(this.chartVersion, {
+        packages: ["timeline"],
+        language: this.chartLanguage
+      });
+      google.charts.setOnLoadCallback(dojoLang.hitch(this, function() {
+        this._setChartOptions();
+        this._getChartData();
+      }));
+    },
+
+    //Set the charts options based on the widget config in the Mx Modeler
+    _setChartOptions: function() {
+      logger.debug(this.id + "._setChartOptions");
+      this._options = {
+        timeline: {
+          colorByRowLabel: this.colorByRowLabel,
+          groupByRowLabel: this.groupByRowLabel,
+          showRowLabels: this.showRowLabels,
+          showBarLabels: this.showBarLabels,
+          singleColor: (this.barColor && this.barColor.trim().length ? this.barColor : null)
+        },
+        tooltip: {
+          trigger: (this.showTooltips == true ? 'focus' : 'none')
+        },
+        backgroundColor: (this.backgroundColor && this.backgroundColor.trim().length ? this.backgroundColor : null)
+      };
+    },
+
+    //Execute the provided microflow in Mendix to get the display data
+    _getChartData: function() {
+      logger.debug(this.id + "._getChartData");
+      mx.data.action({
+        params: {
+          applyto: "selection",
+          actionname: this.dataRetrieveMF,
+          guids: [this._contextObj.getGuid()]
+        },
+        store: {
+          caller: this.mxform
+        },
+        callback: dojoLang.hitch(this, this._buildDataTable),
+        error: dojoLang.hitch(this, function(error) {
+          console.log(this.id + '_getChartData ' + error);
+        })
+      });
+    },
+
+    //Enter the data into the Google Chart format
+    _buildDataTable: function(itemList) {
+      logger.debug(this.id + "._buildDataTable");
+      var
+        customTooltip = this.tooltip && this.tooltip.trim().length,
+        totalRows = itemList.length,
+        totalToLoad = (customTooltip ? totalRows * 5 : totalRows * 4);
+
+      this._dataTable = new google.visualization.DataTable();
+      this._dataTable.addRows(totalRows);
+      this._rowItems = [];
+      this._itemsLoaded = 0;
+
+      this._dataTable.addColumn({
+        type: 'string',
+        id: 'rowLabel'
+      });
+      this._dataTable.addColumn({
+        type: 'string',
+        id: 'barLabel'
+      });
+      //Add the optional tooltip if it was defined
+      if (customTooltip) {
+        this._dataTable.addColumn({
+          type: 'string',
+          role: 'Tooltip',
+          p: {
+            html: true
+          }
+        });
+      }
+      this._dataTable.addColumn({
+        type: 'date',
+        id: 'Start'
+      });
+      this._dataTable.addColumn({
+        type: 'date',
+        id: 'End'
+      });
+
+      dojoArray.forEach(itemList, dojoLang.hitch(this, function(item, i) {
+        var row = [];
+        this._rowItems[i] = item.getGuid();
+
+        item.fetch(this.rowLabel, dojoLang.hitch(this, function(value) {
+          this._dataTable.setValue(i, 0, value);
+          this._chartDataLoaded(totalToLoad);
+        }));
+        item.fetch(this.barLabel, dojoLang.hitch(this, function(value) {
+          this._dataTable.setValue(i, 1, value);
+          this._chartDataLoaded(totalToLoad);
+        }));
+        //Add the optional tooltip if it was defined
+        if (customTooltip) {
+          item.fetch(this.tooltip, dojoLang.hitch(this, function(value) {
+            this._dataTable.setValue(i, 2, value);
+            this._chartDataLoaded(totalToLoad);
+          }));
+        }
+        item.fetch(this.barStart, dojoLang.hitch(this, function(value) {
+          this._dataTable.setValue(i, (customTooltip ? 3 : 2), new Date(value));
+          this._chartDataLoaded(totalToLoad);
+        }));
+        item.fetch(this.barEnd, dojoLang.hitch(this, function(value) {
+          this._dataTable.setValue(i, (customTooltip ? 4 : 3), new Date(value));
+          this._chartDataLoaded(totalToLoad);
+        }));
+      }));
+
+    },
+
+    //Check if all the data has been loaded
+    _chartDataLoaded: function(totalToLoad) {
+      logger.debug(this.id + "._chartDataLoaded");
+      this._itemsLoaded = this._itemsLoaded + 1;
+      if (this._itemsLoaded === totalToLoad) {
+        this._drawChart();
+      }
+    },
+
+    //Handle user interaction (Click on bar)
+    _selectHandler: function() {
+      logger.debug(this.id + "._selectHandler");
+      var selectedDataItem = this._chart.getSelection()[0];
+      if (selectedDataItem) {
+        mx.data.action({
+          params: {
+            applyto: "selection",
+            actionname: this.onSelectMF,
+            guids: [this._rowItems[selectedDataItem.row]]
+          },
+          store: {
+            caller: this.mxform
+          },
+          error: dojoLang.hitch(this, function(error) {
+            console.log(this.id + '_selectHandler ' + error);
+          })
+        });
+      }
+    },
+
+    //Draw the chart on screen
+    _drawChart: function() {
+      logger.debug(this.id + "._drawChart");
+      if (this._chart) {
+        this._chart.clearChart();
+        dojoStyle.set(this.domNode, "height", this.maxHeight + 'px');
+      } else {
+        this._chart = new google.visualization.Timeline(this.domNode);
+        if (this.onSelectMF && this.onSelectMF.trim().length) {
+          google.visualization.events.addListener(this._chart, 'select', dojoLang.hitch(this, this._selectHandler));
+        }
+        google.visualization.events.addListener(this._chart, 'ready', dojoLang.hitch(this, this._setGraphHeight));
+      }
+      this._chart.draw(this._dataTable, this._options);
+    },
+
+    //Adjust the height of the container to implement max-height
+    _setGraphHeight: function() {
+      var
+        div = dojoQuery('div div', this.domNode)[0],
+        svg = dojoQuery('svg', this.domNode),
+        g1 = dojoQuery('g', svg[0])[0],
+        g2 = (svg.length == 1 ? dojoQuery('g', svg[0])[1] : dojoQuery('g', svg[1])[0]),
+        graphHeight = g1.getBBox().height + g2.getBBox().height + 25;
+
+      if (dojoStyle.get(this.domNode, 'height') == 0) {
+        return;
+      }
+
+      if (graphHeight < dojoStyle.get(this.domNode, 'height')) {
+        svg[0].setAttribute('height', graphHeight);
+        if (svg[1]) {
+          svg[1].setAttribute('height', graphHeight);
+        }
+        dojoStyle.set(div, 'height', graphHeight + 'px');
+        dojoStyle.set(this.domNode, 'height', graphHeight + 'px');
+      }
+    },
+
+    _unsubscribe: function() {
+      if (this._handles) {
+        dojoArray.forEach(this._handles, function(handle) {
+          mx.data.unsubscribe(handle);
+        });
+        this._handles = [];
+      }
+    },
+
+    // Reset subscriptions.
+    _resetSubscriptions: function() {
+      logger.debug(this.id + "._resetSubscriptions");
+      // Release handles on previous object, if any.
+      this._unsubscribe();
+
+      // When a mendix object exists create subscribtions.
+      if (this._contextObj) {
+        var objectHandle = mx.data.subscribe({
+          guid: this._contextObj.getGuid(),
+          callback: dojoLang.hitch(this, function(guid) {
+            this._updateRendering();
+          })
+        });
+
+        this._handles = [objectHandle];
+      }
+    }
+  });
+});
+
+require(["GoogleChartTimeline/widget/GoogleChartTimeline"]);
